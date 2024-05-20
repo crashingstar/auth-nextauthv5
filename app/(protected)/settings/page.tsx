4ff97412956ca2caedf1acc9ee973f51f68a399e
unknown <junhao12345@hotmail.com>
2024-05-21 00:08:22 +0800

    Use callbacks to add user id and role in session

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -7,7 +7,7 @@ const SettingsPage = async () => {
         {JSON.stringify(session)}
         <form action={async () =>{
             "use server"
-            await signOut()
+            await signOut({redirectTo: '/auth/login'})
         }}>
             <button type='submit'>
                 Signout
